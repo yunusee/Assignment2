d544a7d1ef5c95fad15be10e3b9a9ec5659a1dae
Başar Çubukçuoğlu <basarcubukcuoglu@gmail.com>
2023-09-20 16:48:16 +0300

    Array Create method added

--- ArrayAssignment.java
+++ ArrayAssignment.java
@@ -1,2 +1,14 @@
+import java.util.Arrays;
+import java.util.Random;
+
 public class ArrayAssignment {
+    public static int[] createArray(int num){
+        int[] array = new int[num];
+        Random random = new Random();
+    
+        for(int i = 0; i < array.length; i++){
+          array[i] = random.nextInt(101);
+        } 
+        return array;
+      }
 }
